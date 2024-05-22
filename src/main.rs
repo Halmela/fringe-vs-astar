e3e23fceb22591867965eea54cbace80ccff15f6
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-05-22 19:44:05 +0300

    very big benchmarks

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,34 @@ use fringe_vs_astar::pathfinder::*;
 use std::collections::HashSet;
 
 fn main() -> anyhow::Result<()> {
-    let map = "benchmarks/Berlin_1_512.map";
+    let map = "benchmarks/scene_sp_sax_04.map";
     let grid = map_builder(map, MapType::GridMap)?;
     let grid_lol = map_builder(map, MapType::GridMap)?;
-    println!("{}", grid);
+    //println!("{}", grid);
 
     //let graph = AdjacencyMapGraph::new(grid);
     let graph = AdjacencyGridGraph::new(grid);
 
-    println!("expected: 768.02142791");
-    let path: HashSet<(usize, usize)> = a_star_simple(510, 66, 21, 507, graph)
+    println!("expected: 7712.59278044");
+    let path: HashSet<(usize, usize)> = a_star_simple(3411, 4166, 3381, 533, graph)
         .unwrap()
         .drain(..)
         .collect();
 
-    let mut result = String::new();
-    for y in 0..grid_lol.get_height() {
-        for x in 0..grid_lol.get_width() {
-            if path.contains(&(x, y)) {
-                result.push('🟩');
-            } else if let Some(true) = grid_lol.get_cell(x, y) {
-                result.push('⬛');
-            } else {
-                result.push('⬜');
-            }
-        }
-        result.push('\n');
-    }
-    println!("{}", result);
+    // let mut result = String::new();
+    // for y in 0..grid_lol.get_height() {
+    //     for x in 0..grid_lol.get_width() {
+    //         if path.contains(&(x, y)) {
+    //             result.push('🟩');
+    //         } else if let Some(true) = grid_lol.get_cell(x, y) {
+    //             result.push('⬛');
+    //         } else {
+    //             result.push('⬜');
+    //         }
+    //     }
+    //     result.push('\n');
+    // }
+    // println!("{}", result);
 
     Ok(())
 }
