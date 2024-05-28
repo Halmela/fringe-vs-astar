62682d0ae3d7d71d7e4569e14366f78451375bdc
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-05-28 17:12:34 +0300

    restructure context and add timing support

--- src/context.rs
+++ src/context.rs
@@ -11,14 +11,15 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
+use std::time::Instant;
 
 /// Holds all relevant information of map and problems and handles pathfinders
 pub struct Context {
     map: Box<dyn Map>,
     graph: Box<dyn Graph>,
-    problem: Option<Problem>,
+    problems: Vec<Problem>,
     mode: Mode,
-    print_level: u8,
+    print_level: usize,
 }
 
 impl Context {
@@ -41,89 +42,106 @@ impl Context {
         let mut context = Context {
             map,
             graph,
-            problem: None,
+            problems: vec![],
             mode: cli.mode,
-            print_level: cli.silent,
+            print_level: cli.silent as usize,
         };
 
-        let mut problem_file = Default::default();
-        if let Some(problem) = cli.problem_file {
-            problem_file = problem;
-        } else {
-            problem_file = cli.map_file;
-            problem_file.set_extension("map.scenario");
-            if !problem_file.as_path().try_exists().is_ok_and(|b| b) {
-                problem_file.set_extension("scen");
-                if !problem_file.as_path().try_exists().is_ok_and(|b| b) {
-                    println!("Could not find a default problem file for map with extensions .scenario and .scen");
-                    return;
-                }
-            }
-        }
-        if context.print_level <= 2 {
-            if let Some(s) = problem_file.to_str() {
-                println!("Using scenario file {s}");
-            }
-        }
-
-        if let Some(n) = cli.problem_number {
-            println!("Loading problem number {n}");
-            context
-                .read_problem_from_file(&problem_file, n)
-                .expect("Could not find a problem with supplied number");
-        }
+        context
+            .load_problems(cli.problem_file, cli.map_file, cli.problem_number)
+            .expect("Error loading problems");
 
         match context.mode {
-            Mode::Print => match (context.print_level, context.problem.as_ref()) {
-                (0, Some(_)) => {
-                    context.print_problem();
-                }
-                (0, None) => {
-                    println!("{}", context.map);
-                }
-                (1, Some(_)) => {
-                    println!("{}", context.problem.as_ref().unwrap());
-                }
-                (1, None) => {
-                    println!(
-                        "Map width: {}\n    height: {}",
-                        context.map.get_width(),
-                        context.map.get_height()
-                    );
-                }
-                _ => {}
-            },
-
-            Mode::AStar => {
-                if context.print_level <= 2 {
-                    println!("Solving using A*");
-                }
-                if context.problem.is_some() {
-                    context.solve();
-                } else {
-                    context
-                        .run_full_file(problem_file)
-                        .expect("something went wrong running the file");
-                }
+            Mode::Print => {
+                context.print_mode();
             }
-            Mode::Fringe => {
-                if context.print_level <= 2 {
-                    println!("Solving using Fringe search");
-                }
-                if context.problem.is_some() {
-                    context.solve();
-                } else {
-                    context
-                        .run_full_file(problem_file)
-                        .expect("something went wrong running the file");
-                }
+            _ => {
+                context.solve_mode();
             }
         }
     }
 
+    fn solve_mode(mut self) {
+        if self.print_level <= 2 {
+            if matches!(self.mode, Mode::AStar) {
+                println!("Solving using A*");
+            } else {
+                println!("Solving using Fringe search");
+            }
+        }
+
+        if self.problems.len() == 0 {
+            panic!("No problems to solve")
+        } else if self.problems.len() == 1 {
+            self.solve(0);
+        } else {
+            self.solve_full();
+        }
+    }
+
+    fn print_mode(self) {
+        match (self.print_level, self.problems.len()) {
+            (0, 0) => {
+                println!("{}", self.map);
+            }
+            (0, _) => {
+                self.print_problems();
+            }
+            (1, 0) => {
+                println!(
+                    "Map width: {}\n    height: {}",
+                    self.map.get_width(),
+                    self.map.get_height()
+                );
+            }
+            (1, _) => {
+                for problem in self.problems {
+                    println!("{}", problem);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn load_problems(
+        &mut self,
+        problem_file: Option<PathBuf>,
+        map_file: PathBuf,
+        problem_number: Option<usize>,
+    ) -> anyhow::Result<()> {
+        let mut path = Default::default();
+
+        if let Some(problem) = problem_file {
+            path = problem;
+        } else {
+            path = map_file;
+            path.set_extension("map.scenario");
+            if !path.as_path().try_exists().is_ok_and(|b| b) {
+                path.set_extension("scen");
+                if !path.as_path().try_exists().is_ok_and(|b| b) {
+                    panic!("Could not find a default problem file for map with extensions .scenario and .scen");
+                }
+            }
+        }
+        if self.print_level <= 2 {
+            if let Some(s) = path.to_str() {
+                println!("Using scenario file {s}");
+            }
+        }
+
+        if let Some(n) = problem_number {
+            println!("Loading problem number {n}");
+            self.read_problem_from_file(&path, n)
+                .expect("Could not find a problem with supplied number");
+        } else {
+            self.read_full_problem_file(path)?;
+        }
+        Ok(())
+    }
+
     /// Set problem
     pub fn set_problem(&mut self, problem: Problem) {
-        self.problem = Some(problem);
+        self.problems = vec![problem];
     }
 
     /// Read `n`th (INDEXING STARTS FROM 1!!!) problem from file to the struct.
@@ -134,147 +152,176 @@ impl Context {
     ) -> anyhow::Result<()> {
         let f = File::open(problem_file)?;
         let mut content = BufReader::new(f).lines();
-        self.set_problem(Problem::parse_problem(content.nth(problem).unwrap()?)?);
+        self.set_problem(Problem::parse_problem(
+            content.nth(problem).unwrap()?,
+            problem,
+        )?);
 
         Ok(())
     }
 
-    /// Read a problem file and run everyone. `print` handles if results should be printed with a map
-    pub fn run_full_file(&mut self, file_path: PathBuf) -> anyhow::Result<()> {
+    pub fn read_full_problem_file(&mut self, file_path: PathBuf) -> anyhow::Result<()> {
         let f = File::open(file_path)?;
-        let mut content = BufReader::new(f).lines();
+        let mut content = BufReader::new(f).lines().enumerate();
         content.next();
         let problems: Vec<Problem> = content
-            .map(|row| Problem::parse_problem(row.unwrap()))
+            .map(|(i, row)| {
+                Problem::parse_problem(row.expect(&format!("Error parsing problem {}", i)), i)
+            })
             .flatten() // haha lets just get rid of the errors
             .collect();
+        self.problems = problems;
+        Ok(())
+    }
 
+    pub fn solve_full(&mut self) {
         let mut error = 0.0;
         let mut len = 0.0;
 
         if self.print_level <= 2 {
-            println!("Solving {} problems...", problems.len());
+            println!("Solving {} problems...", self.problems.len());
         }
 
-        for problem in problems {
+        for (i, problem) in self.problems.iter().enumerate() {
+            let result = self
+                .solve(i)
+                .expect(&format!("Could not find solution for:\n{}", problem));
             let expected = problem.length;
             len += expected.map_or_else(|| 0.0, |_| 1.0);
-            self.set_problem(problem);
-
-            if let Some(result) = self.solve() {
-                error += (result - expected.unwrap_or_else(|| f64::MAX)).abs();
+            if let Some(expected) = problem.length {
+                len += 1.0;
+                error += (result - expected).abs();
             }
         }
         if self.print_level <= 2 {
             println!("Average error: {}", error / len);
         }
-
-        Ok(())
     }
 
     /// Solve currently loaded problem. `full_print` handles if results should be printed with a map
-    pub fn solve(&self) -> Option<f64> {
-        if let Some(Problem {
+    pub fn solve(&self, problem: usize) -> Option<f64> {
+        if self.print_level <= 1 {
+            println!("{}", self.problems[problem]);
+        }
+        let Problem {
             start_x,
             start_y,
             goal_x,
             goal_y,
             ..
-        }) = self.problem.as_ref()
-        {
-            if self.print_level <= 1 {
-                println!("{}", self.problem.as_ref().unwrap());
-            }
-            let mut solution = None;
-            match self.mode {
-                Mode::AStar => {
-                    let astar = AStar::new(*start_x, *start_y, *goal_x, *goal_y, &self.graph);
-                    solution = astar.solve();
-                }
-                Mode::Fringe => {
-                    let fringe =
-                        FringeSearch::new(*start_x, *start_y, *goal_x, *goal_y, &self.graph);
-                    solution = fringe.solve();
-                }
-                _ => {
-                    println!("We can't solve in print mode");
-                    return None;
-                }
-            }
+        } = self.problems[problem];
 
-            if let Some((path, length)) = solution {
-                self.print_solution(path, length);
-                return Some(length);
-            } else {
-                println!("No path found");
+        let mut solution = None;
+        match self.mode {
+            Mode::AStar => {
+                let now = Instant::now();
+
+                let astar = AStar::new(start_x, start_y, goal_x, goal_y, &self.graph);
+                solution = astar.solve();
+
+                let done = Instant::now();
+                let duration = done.checked_duration_since(now);
+                if self.print_level <= 2 {
+                    if let Some(d) = duration {
+                        println!("Solved in {:?}", d);
+                    } else {
+                        println!("Error in timing");
+                    }
+                }
+            }
+            Mode::Fringe => {
+                let now = Instant::now();
+                let fringe = FringeSearch::new(start_x, start_y, goal_x, goal_y, &self.graph);
+                solution = fringe.solve();
+
+                let done = Instant::now();
+                let duration = done.checked_duration_since(now);
+                if self.print_level <= 2 {
+                    if let Some(d) = duration {
+                        println!("Solved in {:?}", d);
+                    } else {
+                        println!("Error in timing");
+                    }
+                }
+            }
+            _ => {
+                println!("We can't solve in print mode");
                 return None;
             }
         }
-        None
+
+        if let Some((path, length)) = solution {
+            self.print_solution(path, length, problem);
+            return Some(length);
+        } else {
+            println!("No path found");
+            return None;
+        }
     }
 
     /// Print solution, `full` specifies if map is printed
-    fn print_solution(&self, mut path: Vec<(usize, usize)>, path_length: f64) {
-        if let Some(Problem {
+    fn print_solution(&self, mut path: Vec<(usize, usize)>, path_length: f64, problem: usize) {
+        let Problem {
             start_x,
             start_y,
             goal_x,
             goal_y,
             length,
-        }) = self.problem.as_ref()
-        {
-            if self.print_level == 0 {
-                let path: HashSet<(usize, usize)> = path.drain(..).collect();
-                let mut result = String::new();
-                for y in 0..self.map.get_height() {
-                    for x in 0..self.map.get_width() {
-                        if (&x, &y) == (start_x, start_y) {
-                            result.push('🏁');
-                        } else if (&x, &y) == (goal_x, goal_y) {
-                            result.push('🏆');
-                        } else if path.contains(&(x, y)) {
-                            result.push('🟩');
-                        } else if let Some(true) = self.map.get_cell(x, y) {
-                            result.push('⬛');
-                        } else {
-                            result.push('⬜');
-                        }
+            ..
+        } = self.problems[problem];
+
+        if self.print_level == 0 {
+            let path: HashSet<(usize, usize)> = path.drain(..).collect();
+            let mut result = String::new();
+            for y in 0..self.map.get_height() {
+                for x in 0..self.map.get_width() {
+                    if (x, y) == (start_x, start_y) {
+                        result.push('🏁');
+                    } else if (x, y) == (goal_x, goal_y) {
+                        result.push('🏆');
+                    } else if path.contains(&(x, y)) {
+                        result.push('🟩');
+                    } else if let Some(true) = self.map.get_cell(x, y) {
+                        result.push('⬛');
+                    } else {
+                        result.push('⬜');
                     }
-                    result.push('\n');
                 }
-                println!("{}", result);
-                println!("{}", self.problem.as_ref().unwrap());
-                println!("Result:\n\t{}", path_length);
-                if let Some(l) = length {
-                    println!("Difference:\n\t{}\n", path_length - l);
-                }
-            } else if self.print_level == 1 {
-                println!("Result:\n\t{}", path_length);
-                if let Some(l) = length {
-                    println!("Difference:\n\t{}\n", path_length - l);
-                }
+                result.push('\n');
             }
-        } else {
-            println!("No problem to print\n");
+            println!("{}", result);
+            println!("{}", self.problems[problem]);
+            println!("Result:\n\t{}", path_length);
+            if let Some(l) = length {
+                println!("Difference:\n\t{}\n", path_length - l);
+            }
+        } else if self.print_level == 1 {
+            println!("Result:\n\t{}", path_length);
+            if let Some(l) = length {
+                println!("Difference:\n\t{}\n", path_length - l);
+            }
         }
     }
 
-    /// Print a problem as a map
-    pub fn print_problem(&self) {
-        if let Some(Problem {
-            start_x,
-            start_y,
-            goal_x,
-            goal_y,
-            ..
-        }) = self.problem.as_ref()
+    /// Print a problems in a map
+    pub fn print_problems(&self) {
+        for (
+            i,
+            Problem {
+                start_x,
+                start_y,
+                goal_x,
+                goal_y,
+                ..
+            },
+        ) in self.problems.iter().enumerate()
         {
             let mut result = String::new();
             for y in 0..self.map.get_height() {
                 for x in 0..self.map.get_width() {
-                    if (&x, &y) == (start_x, start_y) {
+                    if (x, y) == (*start_x, *start_y) {
                         result.push('🏁');
-                    } else if (&x, &y) == (goal_x, goal_y) {
+                    } else if (x, y) == (*goal_x, *goal_y) {
                         result.push('🏆');
                     } else if let Some(true) = self.map.get_cell(x, y) {
                         result.push('⬛');
@@ -285,9 +332,7 @@ impl Context {
                 result.push('\n');
             }
             println!("{}", result);
-            println!("{}\n", self.problem.as_ref().unwrap());
-        } else {
-            println!("No problem to print\n");
+            println!("{}\n", self.problems[i]);
         }
     }
 }
--- src/problem.rs
+++ src/problem.rs
@@ -7,6 +7,7 @@ pub struct Problem {
     pub goal_x: usize,
     pub goal_y: usize,
     pub length: Option<f64>,
+    pub number: usize,
 }
 
 impl Problem {
@@ -17,6 +18,7 @@ impl Problem {
         goal_x: usize,
         goal_y: usize,
         length: Option<f64>,
+        number: usize,
     ) -> Problem {
         Problem {
             start_x,
@@ -24,11 +26,12 @@ impl Problem {
             goal_x,
             goal_y,
             length,
+            number,
         }
     }
 
     /// Parse `.scenario` row as a problem
-    pub fn parse_problem(value: String) -> anyhow::Result<Problem> {
+    pub fn parse_problem(value: String, number: usize) -> anyhow::Result<Problem> {
         let fields: Vec<&str> = value.split_ascii_whitespace().collect();
         let wanted = fields.split_at(fields.len() - 5).1;
 
@@ -38,6 +41,7 @@ impl Problem {
             goal_x: wanted[2].parse()?,
             goal_y: wanted[3].parse()?,
             length: Some(wanted[4].parse()?),
+            number,
         })
     }
 }
@@ -45,7 +49,7 @@ impl Problem {
 impl fmt::Display for Problem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut result = String::new();
-        result.push_str("Problem:\n");
+        result.push_str(&format!("Problem {}:\n", self.number));
         result.push_str(&format!("\t({}, {}) -> ", self.start_x, self.start_y));
         result.push_str(&format!("({}, {})", self.goal_x, self.goal_y));
         result.push_str("\nExpected cost:\n");
