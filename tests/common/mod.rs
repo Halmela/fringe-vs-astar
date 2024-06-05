84f31b01bea5101a0bf55fa541273142eb36e8a3
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-06-05 12:46:35 +0300

    rename benchmarks directory to maps

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,7 +4,7 @@ use fringe_vs_astar::context::*;
 use clap::Parser;
 
 pub fn full_lak104d_context(mode: &str) -> Context {
-    let arguments = vec!["-sss", mode, "benchmarks/lak104d.map"];
+    let arguments = vec!["-sss", mode, "maps/lak104d.map"];
     let cli = Cli::parse_from(arguments.iter());
     Context::new(cli)
 }
