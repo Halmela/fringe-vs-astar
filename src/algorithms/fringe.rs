cc0f526b9803de6f645275f1335929410476da94
Halmela Mikko O <mikko.halmela@helsinki.fi>
2024-06-18 20:30:40 +0300

    Some doc comments

--- src/algorithms/fringe.rs
+++ src/algorithms/fringe.rs
@@ -7,12 +7,15 @@ use crate::Node;
 
 mod cache;
 
+/// Ugly wrapper for the common heuristic function. Handles 1D -> 2D coordinate conversion...
+/// This should not be a thing anymore...
 struct Heuristic {
     goal: (usize, usize),
     width: usize,
 }
 
 impl Heuristic {
+    /// Initialize self with goal and width
     pub fn new(goal: usize, width: usize) -> Self {
         Heuristic {
             goal: index_to_xy(goal, width),
@@ -20,20 +23,27 @@ impl Heuristic {
         }
     }
 
+    /// Calculate heuristic value
     pub fn get(&self, i: Node) -> f32 {
         heuristic(index_to_xy(i as usize, self.width), self.goal)
     }
 }
 
+/// Fringe search implementation.
+/// Fringe holds now- and later-queues and Cache holds information on nodes.
+/// This structure kind of glues those two together and passes nodes around as needed.
+/// I wanted to separate them away from this main search algorithm for clarity, since caching can be messy
+/// and at least in some points of development the queue handling was too.
 pub struct FringeSearch<'a> {
     fringe: Fringe,
-    cache: Cache, // (cost,parent,heuristic,in_fringe)
+    cache: Cache,
     start: Node,
     goal: Node,
     graph: &'a AdjacencyListGraph,
 }
 
 impl<'a> FringeSearch<'a> {
+    /// Initialize the search with a start, goal and a graph to be acted upon.
     pub fn new(start: usize, goal: usize, graph: &'a AdjacencyListGraph) -> Self {
         let size = graph.get_width() * graph.get_height();
         let fringe = Fringe::new(start.try_into().unwrap(), size);
@@ -49,6 +59,12 @@ impl<'a> FringeSearch<'a> {
         }
     }
 
+    /// Solve from start to goal. Returns `Some((path,cost))` if it can be found, `None` if path can't be found.
+    /// I would like to unroll this main loop at some point to expose the datastructures at different points of search.
+    ///
+    /// Main idea here is to get a new node from now-queue, process it and maybe return it.
+    /// If now is empty, then try to prepare datastructures for next iteration (f_min -> f_limit and later -> now).
+    /// If now is empty and later is empty, then no further search can be conducted and thus a path can be found and `None` is returned.
     pub fn solve(mut self) -> Option<(Vec<usize>, f32)> {
         loop {
             if let Some(node) = self.fringe.pop_now() {
@@ -64,6 +80,9 @@ impl<'a> FringeSearch<'a> {
         }
     }
 
+    /// Check from cache if a `Node` has a low enough cost to have it (as a goal) returned,
+    /// or have its neighbors processed.
+    /// If it has too high of a cost, it is then put to later if it is not already there.
     fn process_node(&mut self, node: Node) -> Option<Node> {
         match self.cache.check_estimate(node) {
             Action::Process(goal) if goal == self.goal => return Some(goal),
@@ -78,11 +97,13 @@ impl<'a> FringeSearch<'a> {
         None
     }
 
+    /// Prepare the datastructures for next iteration
     fn prepare_next_iteration(&mut self) -> bool {
         self.cache.refresh_limits();
         self.fringe.later_to_now()
     }
 
+    /// Put node's viable neighbors to the now-queue. Viable as in lower cost then ever seen before.
     fn process_neighbors(&mut self, node: Node) {
         self.graph
             .neighbors(node as usize)
--- src/algorithms/fringe/cache.rs
+++ src/algorithms/fringe/cache.rs
@@ -4,6 +4,9 @@ use super::Heuristic;
 
 use std::ops::{Index, IndexMut};
 
+/// Stored values for a single node in graph as wanted by Fringe Search.
+/// Cost, estimate and parent are updated as needed, heuristic is calculated only once
+/// and later makes sure that each node is put to later-queue at most once per iteration.
 #[derive(Clone, Copy, Debug)]
 pub struct CacheValue {
     pub cost: f32,
@@ -14,6 +17,7 @@ pub struct CacheValue {
 }
 
 impl CacheValue {
+    /// Initialize cache value
     pub fn new() -> Self {
         CacheValue {
             cost: f32::MAX,
@@ -25,12 +29,16 @@ impl CacheValue {
     }
 }
 
+/// What should the main algorithm do with a node?
 pub enum Action {
     Process(Node),
     ToLater(Node),
     Nothing,
 }
 
+/// Datastructure for caching information while performing Fringe search.
+/// I separated this to its own structure to clarify the main algorithm.
+/// This does the book keeping for every node and updates the values as needed.
 pub struct Cache {
     pub cache: Vec<CacheValue>,
     heuristic: Heuristic,
@@ -40,6 +48,7 @@ pub struct Cache {
 }
 
 impl Cache {
+    /// Initialize cache
     pub fn new(start: usize, size: usize, heuristic: Heuristic) -> Self {
         let mut cache: Vec<CacheValue> = vec![CacheValue::new(); size];
         let f_limit = heuristic.get(start.try_into().unwrap());
@@ -55,6 +64,8 @@ impl Cache {
         }
     }
 
+    /// Decide what the algorithm should do for a given node.
+    /// This is the most used function in the search, since every node popped from now passes through this
     #[inline(always)]
     pub fn check_estimate(&mut self, node: Node) -> Action {
         let estimate = self[node].estimate;
@@ -74,12 +85,14 @@ impl Cache {
         }
     }
 
+    /// Update value of a node
     pub fn update(&mut self, node: Node, cost: f32, parent: Node) {
         self[node].cost = cost;
         self[node].parent = parent;
         self[node].estimate = cost + self.get_heuristic(node);
     }
 
+    /// Get heuristic value from cache or calculate it
     pub fn get_heuristic(&mut self, node: Node) -> f32 {
         if self[node].heuristic == f32::MAX {
             self[node].heuristic = self.heuristic.get(node);
@@ -87,32 +100,34 @@ impl Cache {
         self[node].heuristic
     }
 
+    /// Get cost of a node
     pub fn get_cost(&self, node: Node) -> f32 {
         self[node].cost
     }
 
+    /// Update f_limit value and +1 to the iteration count used for counting if a node is in later
     pub fn refresh_limits(&mut self) {
         self.f_limit = self.f_min;
         self.f_min = f32::MAX;
         self.iteration += 1;
     }
 
+    /// Decide if a child-node should be added to the now-queue.
+    /// It's value is updated, if it is added.
+    /// This returns `Option<Node` because it allows neat filter_map on the call site.
     pub fn check(&mut self, child: &usize, parent: Node, move_cost: f32) -> Option<Node> {
         let new_cost = self[parent].cost + move_cost;
         let child = (*child).try_into().unwrap();
 
         if new_cost < self[child].cost {
             self.update(child, new_cost, parent);
-            // if new_cost <= self.f_min {
             Some(child)
-            /* } else {
-                self[child].later = self.iteration;
-                None
-            } */
         } else {
             None
         }
     }
+
+    /// Check if a node is already in later and mark it to be there if it already wasn't
     fn not_in_later(&mut self, node: Node) -> bool {
         if self[node].later != self.iteration {
             self[node].later = self.iteration;
